a5c8b7a8be6a465da1e0d6886777e1eaeb201a31
Chanpitou Um <umchanpitou@gmail.com>
2026-05-27 18:41:29 +0000

    updated staging source v2

--- models/staging/stg_raw_census.sql
+++ models/staging/stg_raw_census.sql
@@ -1,13 +1,28 @@
-SELECT
-    county_name,
-    total_population,
-    median_income,
-    bachelors_degree,
-    below_poverty,
-    median_home_value,
-    median_rent,
-    high_school_grad,
-    state_code,
-    county_code,
-    loaded_at
-FROM {{ source('raw_source', 'raw_census') }}
+with 
+
+source as (
+
+    select * from {{ source('raw_source', 'raw_census') }}
+
+),
+
+renamed as (
+
+    select
+        county_name,
+        total_population,
+        median_income,
+        bachelors_degree,
+        below_poverty,
+        median_home_value,
+        median_rent,
+        high_school_grad,
+        state_code,
+        county_code,
+        loaded_at
+
+    from source
+
+)
+
+select * from renamed
